3b71fe2680de15748e2b924a716a835096444ff1
xivlives <94845985+xivlives@users.noreply.github.com>
2024-08-13 17:49:00 +0100

    added components

---
+++ client/src/app/components/landingPage/navbar.js
@@ -0,0 +1,23 @@
+import * as react from 'react';
+import Image from 'next/image';
+
+export default function Navbar() {
+    return(
+        <nav className='bg-black'>
+            <div className='max-md-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+                <div className='flex items-center justify-between h-16'>
+                    <div className='flex items-center'>
+                        <div className='flex-shrink-0'>
+                            <a href='/' className='text-white font-extrabold'>PREPBOT</a>
+                        </div>
+                    </div>
+                    <div className='hidden md:block'>
+                        <div className='ml-4 flex items-center space-x-4'>
+                            <a href='/' className='text-white font-medium hover:bg-gray-400 hover:text-black'>Waitlist now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
