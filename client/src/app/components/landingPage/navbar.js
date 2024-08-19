93cf687e26bf00f8070df755ec87e36acd5f9ccb
xivlives <94845985+xivlives@users.noreply.github.com>
2024-08-19 23:48:04 +0100

    commit

--- client/src/app/components/landingPage/navbar.js
+++ client/src/app/components/landingPage/navbar.js
@@ -8,14 +8,14 @@ export default function Navbar() {
                 <div className='flex items-center justify-between h-16'>
                     <div className='flex items-center'>
                         <div className='flex-shrink-0'>
-                          <span className="text-3xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-pink-500">
-                            Preppy
+                          <span className="text-4xl font-extrabold text-transparent font-mono bg-clip-text bg-gradient-to-r from-cyan-500 to-pink-500">
+                            PREPPYY
                           </span>
                         </div>
                     </div>
                     <div className='hidden md:block'>
                         <div className='ml-4 flex items-center space-x-4'>
-                            <a href='/' className='text-white font-medium p-3 rounded-md hover:bg-cyan-500 hover:text-black transition duration-300 ease-in-out'>Waitlist now!</a>
+                            <a href='/' className='text-white font-medium p-3 rounded-md hover:bg-cyan-500 hover:text-black transition duration-300 ease-in-out'>Join Now!</a>
                         </div>
                     </div>
                 </div>
