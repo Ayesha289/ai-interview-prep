ca060407ec63c063efc86cf143cbe8c2077f5ead
xivlives <94845985+xivlives@users.noreply.github.com>
2024-08-26 22:20:46 +0100

    bug fixes

--- client/src/app/components/landingPage/navbar.js
+++ client/src/app/components/landingPage/navbar.js
@@ -1,25 +1,48 @@
-import * as React from 'react';
-import Image from 'next/image';
+"use client";
+import * as React from "react";
+import Image from "next/image";
+import Modal from "./modal";
+import { useState, useEffect } from "react";
 
 export default function Navbar() {
-    return(
-        <nav className='bg-black sticky top-0 z-50'>
-            <div className='max-md-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-                <div className='flex items-center justify-between h-16'>
-                    <div className='flex items-center'>
-                        <div className='flex-shrink-0'>
-                          <span className="text-4xl font-extrabold text-transparent font-mono bg-clip-text bg-gradient-to-r from-cyan-500 to-pink-500">
-                            PREPPYY
-                          </span>
-                        </div>
-                    </div>
-                    <div className='hidden md:block'>
-                        <div className='ml-4 flex items-center space-x-4'>
-                            <a href='/' className='text-white font-medium p-3 rounded-md hover:bg-cyan-500 hover:text-black transition duration-300 ease-in-out'>Join Now!</a>
-                        </div>
-                    </div>
-                </div>
+  const [showModal, setShowModal] = useState(false);
+
+  const showDialog = () => {
+    setShowModal(true);
+  };
+
+  const hideDialog = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <nav className="bg-black sticky top-0 z-50">
+      <div className="max-md-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <span className="text-4xl font-extrabold text-transparent font-mono bg-clip-text bg-gradient-to-r from-cyan-500 to-pink-500">
+                PREPPYY
+              </span>
             </div>
-        </nav>
-    )
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-4 flex items-center space-x-4">
+              <button
+                onClick={showDialog}
+                className="text-white font-medium p-3 rounded-md hover:bg-cyan-500 hover:text-black transition duration-300 ease-in-out"
+              >
+                Join Now!
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Modal */}
+      {showModal && (
+        <Modal onClose={hideDialog} />
+      )}
+
+    </nav>
+  );
 }
