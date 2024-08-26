70ad964c26219b1f3162eebe2dc1b5237771fb35
xivlives <94845985+xivlives@users.noreply.github.com>
2024-08-26 22:50:57 +0100

    UI Updates and fixes

--- client/src/app/components/landingPage/navbar.js
+++ client/src/app/components/landingPage/navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-black sticky top-0 z-50">
+    <nav className="bg-black bg-opacity-80 sticky top-0 z-50">
       <div className="max-md-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
