b197550c55c341f2a543df630998de60dc5ce91a
Ayesha289 <ayeshamulani928@gmail.com>
2024-08-30 20:56:32 +0530

    updated home page

--- client/src/app/components/landingPage/MainBody.js
+++ client/src/app/components/landingPage/MainBody.js
@@ -1,9 +1,9 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 'use client'
 import * as React from "react";
-import Image from "next/image";
 import Modal from "./modal";
 import { useState } from 'react';
+import '@/app/globals.css';
 
 export default function body() {
   const explore = () => {
@@ -22,41 +22,25 @@ export default function body() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-tr from-[#0a1f2b] to-black text-white">
-      {/* Header Section */}
-      <div className="text-center py-16">
-        <h1 className="text-5xl font-bold">
+      <div className="text-center py-16 animate-fadeIn">
+        <h1 className="text-6xl font-bold mb-4 transition-transform transform hover:scale-105">
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-pink-500">
-            An Interview PrepBot.
+            Meet Preppyy
           </span>
         </h1>
-        <h2 className="text-4xl font-bold mt-4">Scalable AI.</h2>
-        <p className="text-lg mt-6 max-w-2xl mx-auto">
-          Our technology integrates AI into preparations for any job Interview
-          in any industry. within the coming weeks the product will be live and
-          the first 200 users will be getting full access to the bot.
+        <h2 className="text-3xl font-semibold mb-6">Your AI-Powered Interview Coach</h2>
+        <p className="text-lg max-w-3xl mx-auto leading-relaxed">
+          Elevate your interview game with our advanced AI-driven platform.
+          Get real-time feedback and tailored questions for any role,
+          anywhere.
         </p>
       </div>
 
-      {/* Buttons */}
       <div className="flex space-x-4 mt-8">
-        <button onClick={showDialog} className="py-4 px-2 bg-gradient-to-r from-cyan-500 to-pink-500 rounded-lg">
-          {/* <!-- Button Body --> */}
-          <span className="py-3 px-9 bg-black text-xl rounded-lg">Start Prepping Now</span>
+        <button onClick={showDialog} className="py-3 px-6 bg-gradient-to-r from-cyan-500 to-pink-500 rounded-lg shadow-lg transform transition-transform hover:scale-105">
+          <span className="text-lg text-black font-semibold">Start Prepping Now</span>
         </button>
       </div>
-
-      {/* Image Section
-      <div className="mt-12">
-        <Image
-          src="/assets/logo.png"
-          alt="Blockchain AI Illustration"
-          width={700}
-          height={300}
-          className="w-full h-auto"
-        />
-      </div> */}
-
-      {/* Modal */}
       {showModal && (
         <Modal onClose={hideDialog} />
       )}
