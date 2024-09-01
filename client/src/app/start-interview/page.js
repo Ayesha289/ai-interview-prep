f5200d207215463f1d2b57ae007ed532cab8ef2c
xivlives <94845985+xivlives@users.noreply.github.com>
2024-09-01 01:24:01 +0100

    updated interview session UI and the prompt

--- client/src/app/start-interview/page.js
+++ client/src/app/start-interview/page.js
@@ -56,7 +56,7 @@ export default function StartInterview() {
     } else {
       SpeechRecognition.stopListening();
     }
-  }, [isMicOn]);
+  }, [browserSupportsSpeechRecognition, isMicOn]);
 
   useEffect(() => {
     if (isInitialMount.current) {
